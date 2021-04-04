027cfb2047e2b85a02be06f2693012c7ec23ec8d
Margaret <match14@yandex.ru>
2021-04-04 21:26:27 +0300

    begin page

--- src/js/common.js
+++ src/js/common.js
@@ -1,8 +1,10 @@
 import $ from 'jquery';
+
+var Slider = require("bootstrap-slider");
 // import '@fancyapps/fancybox'
 // import 'bootstrap/js/dist/modal';
 // import 'slick-slider/slick/slick.min.js';
 // import 'select2';
 // import Inputmask from "inputmask";
 
-export {$};
+export {$, Slider};
